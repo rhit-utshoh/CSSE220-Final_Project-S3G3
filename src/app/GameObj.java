f25a804153a59b3f415104c17d8011b5cec8a5b9
rhit-blocksl <blocksl@rose-hulman.edu>
2026-01-29 14:31:36 -0500

    game

---
+++ src/app/GameObj.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.awt.image.BufferedImage;
+
+public class GameObj {
+	int x; 
+	int y; 
+	private static BufferedImage sprite = null;
+	
+	public static void main(String[] args) {
+		
+		
+		}
+
+}
