098871d337979cc298f1d90a590851341f942670
rhit-blocksl <blocksl@rose-hulman.edu>
2026-02-09 23:59:44 -0500

    fixed tiles should work as soon as we figure out game comp vs drawing com
    Signed-off-by: rhit-blocksl <blocksl@rose-hulman.edu>

--- src/model/DrawingComponent.java
+++ src/model/DrawingComponent.java
@@ -12,20 +12,28 @@ import model.Tile;
 
 
 public class DrawingComponent extends JPanel{
+	
 
 	public static final int SIZE = 500;
 	private Tile grassTile, stoneTile;
 	
 	public DrawingComponent() {
-		
+		System.out.println("DrawingComponent.paintComponent called. size=" + getWidth() + "x" + getHeight());
 		setBackground(new Color(72, 150,56));
 		setOpaque(true);
 		setPreferredSize(new Dimension(SIZE, SIZE)); 
-		grassTile = new Tile(SIZE / 10, (SIZE / 10)); 
+		grassTile = new Tile(0, 0, 0); 
+        stoneTile = new Tile(1, 0, 1); 
+
 	}
+	@Override
 	protected void paintComponent(Graphics g) {
 		  super.paintComponent(g);
 		  Graphics2D g2 = (Graphics2D) g;
-		  grassTile.drawOn(g2);
+		  int tileW = getWidth() / 10;
+	      int tileH = getHeight() / 10;
+	      grassTile.drawOn(g2, tileW, tileH);
+	      stoneTile.drawOn(g2, tileW, tileH);
+
 	}
 }
--- src/model/Tile.java
+++ src/model/Tile.java
@@ -5,25 +5,26 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Tile  extends GameObj{
-		private static final int HEIGHT = 50;
-		private static final int WIDTH = 50;
-		private int y;
-		private int x;
-		private Color color = new Color(165, 42, 42); 
+
+	 	private int gridX;
+	    private int gridY;
 		private Rectangle grassTile, brickTile; 
-		private static BufferedImage sprite = null;
+		private static BufferedImage grassSprite;
+		private static BufferedImage stoneSprite;
 		private static boolean triedLoad = false;
+		private int type; 
 		
-		public Tile(int x, int y){
-			this.x = x;
-			this.y = y; 
-			grassTile = new Rectangle(x, y, WIDTH, HEIGHT);
-			brickTile = new Rectangle(x, y, WIDTH, HEIGHT);
+		
+		public Tile(int gridX, int gridY, int type){
+			this.gridX = gridX;
+			this.gridY = gridY; 
+			this.type = type; 
 			loadSpriteOnce();
 		}
 		
@@ -33,29 +34,33 @@ public class Tile  extends GameObj{
 			triedLoad = true;
 			
 			try {
-			// tennis.png must be in the SAME package as Ball.java
-				sprite = ImageIO.read(Tile.class.getResource("grass.jpg"));
-				sprite = ImageIO.read(Tile.class.getResource("stoneTile.png"));
+				grassSprite = ImageIO.read(Tile.class.getResource("/grass.jpg"));
+				stoneSprite = ImageIO.read(Tile.class.getResource("/stoneTile.jpg"));
 			} catch (IOException | IllegalArgumentException ex) {
-					sprite = null; 
+				grassSprite = null; 
+				stoneSprite = null; 
 			}
 		}
 
-		public void drawOn(Graphics2D g2){
+		public void drawOn(Graphics2D g2, int tileW, int tileH){
+			int x = gridX * tileW;
+			int y = gridY * tileH;
+			BufferedImage sprite = (type == 0 ? grassSprite : stoneSprite);
 
 			Shape origionalClip = g2.getClip();
-			Shape rectangle = new Rectangle.Double(x,y,WIDTH, HEIGHT);
+			Shape rectangle = new Rectangle2D.Double(x, y, tileW, tileH);
 			g2.setClip(rectangle);
+			
 			if(sprite != null) {
-				g2.drawImage(sprite,x,y,WIDTH, HEIGHT, null);
+				g2.drawImage(sprite, x, y, tileW, tileH, null);
 			}
-			
-			
-		else {
-			g2.setColor(color);
-			g2.fill(grassTile);
-			g2.fill(brickTile);
+			else {
+				g2.setColor(Color.green);
+				g2.fill(grassTile);
+
+				g2.setColor(Color.DARK_GRAY);
+				g2.fill(brickTile);
 		}
-		}
+	}
 	
 }
