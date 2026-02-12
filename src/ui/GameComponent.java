3a2ee91ce16564a28688c96cc432fd6c4b99a5f8
Ben Wintraub <wintrabj@rose-hulman.edu>
2026-02-12 11:33:13 -0500

    moved GameModel logic to Player + GameComponent

--- src/ui/GameComponent.java
+++ src/ui/GameComponent.java
@@ -1,91 +1,127 @@
 package ui;
 
-import app.GameConfig;
-import model.GameModel;
+import model.GameConfig;
+import model.GameState;
+import model.Player;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
-public class GameComponent extends JComponent {
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-    private final GameModel model;
+import brain.ADHDBrain;
+import brain.NeurotypicalBrain;
 
-    private boolean up, down, left, right;
-
-    public GameComponent(GameModel model) {
-        this.model = model;
-
+public class GameComponent extends JPanel {
+	private final Player p1;
+    private final Player p2;
+    
+    private GameState state = GameState.PLAYING;
+    private int speed = GameConfig.PLAYER_SPEED;
+    
+    private ArrayList<Player> players = new ArrayList<>();
+    
+    private boolean left, right, up, down;
+    private int vx, vy;
+    public GameComponent() {
+    	
+    	setBackground(Color.WHITE);
+    	setOpaque(true);
         setPreferredSize(new Dimension(GameConfig.WIDTH, GameConfig.HEIGHT));
-        setFocusable(true);
-
-        setupKeyBindings();
-
-        Timer timer = new Timer(16, e -> {
-            model.update(up, down, left, right);
-            repaint();
+        
+        p1 = new Player(140, 200, new NeurotypicalBrain());
+        p2 = new Player(260, 200, new ADHDBrain());
+        players.add(p1);
+        players.add(p2);
+        
+        Timer timer = new Timer(50, e -> {
+        	for (Player p : players) {
+        		if (state == GameState.PLAYING)
+        			vx = (right ? GameConfig.PLAYER_SPEED : 0) - (left ? GameConfig.PLAYER_SPEED : 0);
+                	vy = (down ? GameConfig.PLAYER_SPEED : 0) - (up ? GameConfig.PLAYER_SPEED : 0);
+        			p.setVelocity(vx, vy);
+        		p.update();
+        	}
+        	repaint();
         });
         timer.start();
-    }
+        
+        // bind keys
+        setFocusable(true);
+        addKeyListener(ka);
+    }	
 
-    @Override
-    public void addNotify() {
-        super.addNotify();
-        requestFocusInWindow();
-    }
 
-    private void setupKeyBindings() {
-        bind("pressed W", () -> up = true);
-        bind("released W", () -> up = false);
-
-        bind("pressed S", () -> down = true);
-        bind("released S", () -> down = false);
-
-        bind("pressed A", () -> left = true);
-        bind("released A", () -> left = false);
-
-        bind("pressed D", () -> right = true);
-        bind("released D", () -> right = false);
-
-        bind("pressed P", model::togglePause);
-    }
-
-    private void bind(String keyStroke, Runnable action) {
-        InputMap im = getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        ActionMap am = getActionMap();
-
-        im.put(KeyStroke.getKeyStroke(keyStroke), keyStroke);
-        am.put(keyStroke, new AbstractAction() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                action.run();
-            }
-        });
-    }
-
+    KeyAdapter ka = new KeyAdapter() {
+    	@Override
+    	public void keyPressed(KeyEvent e) {
+    		switch (e.getKeyCode()) {
+    		case KeyEvent.VK_W: case KeyEvent.VK_UP:
+    			up = true;
+    			break;
+    		case KeyEvent.VK_S: case KeyEvent.VK_DOWN:
+    			down = true;
+    			break;
+    		case KeyEvent.VK_A: case KeyEvent.VK_LEFT:
+    			left = true;
+    			break;
+    		case KeyEvent.VK_D: case KeyEvent.VK_RIGHT:
+    			right = true;
+    			break;
+    		case KeyEvent.VK_P:
+    			if (state == GameState.PAUSED)
+    				state = GameState.PLAYING;
+    			else
+    				state = GameState.PAUSED;
+    			break;
+    		}
+    	}
+    	@Override
+    	public void keyReleased(KeyEvent e) {
+    		switch (e.getKeyCode()) {
+    		case KeyEvent.VK_UP:
+    			up = false;
+    			break;
+    		case KeyEvent.VK_DOWN:
+    			down = false;
+    			break;
+    		case KeyEvent.VK_LEFT:
+    			left = false;
+    			break;
+    		case KeyEvent.VK_RIGHT:
+    			right = false;
+    			break;
+    		}
+    	}
+    };
+    	
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        //background
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, getWidth(), getHeight());
-
+        Graphics2D g2 = (Graphics2D) g;
+        
         //draw players
         int s = GameConfig.PLAYER_SIZE;
 
         //Player 1 (Neurotypical) - Blue
-        g.setColor(Color.BLUE);
-        g.fillRect(model.getP1().getX(), model.getP1().getY(), s, s);
+        g2.setColor(Color.BLUE);
+        g2.fillRect(p1.getX()-s/2, p1.getY()-s/2, s, s);
 
         //Player 2 (ADHD) - Red
-        g.setColor(Color.RED);
-        g.fillRect(model.getP2().getX(), model.getP2().getY(), s, s);
+        g2.setColor(Color.RED);
+        g2.fillRect(p2.getX()-s/2, p2.getY()-s/2, s, s);
 
         //HUD
-        g.setColor(Color.BLACK);
-        g.drawString("State: " + model.getState() + "   (P = Pause)", 10, 20);
-        g.drawString("WASD = move both players together", 10, 40);
-        g.drawString("P1 Brain: " + model.getP1().getBrainName(), 10, 60);
-        g.drawString("P2 Brain: " + model.getP2().getBrainName() + " (may ignore input sometimes)", 10, 80);
+        g2.setColor(Color.BLACK);
+        g2.drawString("State: " + state + "   (P = Pause)", 10, 20);
+        g2.drawString("WASD = move both players together", 10, 40);
+        g2.drawString("P1 Brain: " + p1.getBrainName(), 10, 60);
+        g2.drawString("P2 Brain: " + p2.getBrainName() + " (may ignore input sometimes)", 10, 80);
     }
 }
